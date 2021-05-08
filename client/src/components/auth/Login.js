c520fe9a2b7c481f5a9dbcb2d551b8b23753faeb
BethanyFolino <bethany.folino@gmail.com>
2021-05-08 13:04:52 -0700

    completed section 8 part 42 - user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
       email: "",
       password: "",
@@ -9,15 +12,20 @@ const Login = () => {
 
     const { email, password } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e => 
+      setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log("SUCCESS");
-        }
-        
-        return (
-          <Fragment>
+        login(email, password);
+    };
+
+    //Redirect if logged in
+    if (isAuthenticated) {
+      return <Redirect to="/dashboard" />;
+    }
+    return (
+        <Fragment>
           <h1 className="large text-primary">Sign In</h1>
           <p className="lead">
               <i className="fas fa-user" /> Sign Into Your Account
@@ -57,5 +65,17 @@ const Login = () => {
     )
   };
 
+  Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
 
-export default Login;
+  const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+  });
+
+
+export default connect(
+  mapStateToProps, 
+  { login }
+)(Login);
