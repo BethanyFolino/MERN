b380eb894b424a970d8b7595ef69a65731af8ae9
BethanyFolino <bethany.folino@gmail.com>
2021-05-11 16:04:17 -0700

    completed section 9 part 48 - Create Profile action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: "",
     website: "",
@@ -36,6 +38,11 @@ const CreateProfile = props => {
   } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  }
 
   return (
     <Fragment>
@@ -147,7 +154,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
 
-}
-
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(null, { createProfile })(withRouter(CreateProfile));
