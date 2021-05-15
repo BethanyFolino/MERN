1ca835b81216e4f08ca748f4c4c649bbe39d539b
BethanyFolino <bethany.folino@gmail.com>
2021-05-14 17:03:44 -0700

    partway through section 9 part 52, moment is throwing deprecation error

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,24 +2,25 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import { deleteEducation } from "../../actions/profile";
 
-const Education = ({ education }) => {
+const Education = ({ education, deleteEducation }) => {
     const educations = education.map(edu => (
-        <td key={edu._id}>
+        <tr key={edu._id}>
             <td>{edu.school}</td>
-            <td className="hide-sm">{exp.title}</td>
+            <td className="hide-sm">{edu.degree}</td>
             <td>
-              <Moment format="YYYY/MM/DD">{edu.degree}</Moment> -{" "}
+              <Moment format="YYYY-MM-DD">{edu.degree}</Moment> -{" "}
               {edu.to === null ? (
                  "Now"
               ) : (
-                <Moment format="YYYY/MM/DD">{edu.to}</Moment>
+                <Moment format="YYYY-MM-DD">{edu.to}</Moment>
               )}
             </td>
             <td>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteEducation(edu._id)} className="btn btn-danger">Delete</button>
             </td>
-        </td>
+        </tr>
     ))
 
     return (
@@ -42,6 +43,7 @@ const Education = ({ education }) => {
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 }
 
-export default Education
+export default connect(null, { deleteEducation })(Education)
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -2,10 +2,11 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import { deleteExperience } from "../../actions/profile";
 
-const Experience = ({ experience }) => {
+const Experience = ({ experience, deleteExperience }) => {
     const experiences = experience.map(exp => (
-        <td key={exp._id}>
+        <tr key={exp._id}>
             <td>{exp.company}</td>
             <td className="hide-sm">{exp.title}</td>
             <td>
@@ -17,9 +18,9 @@ const Experience = ({ experience }) => {
               )}
             </td>
             <td>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteExperience(exp._id)} className="btn btn-danger">Delete</button>
             </td>
-        </td>
+        </tr>
     ))
 
     return (
@@ -42,6 +43,7 @@ const Experience = ({ experience }) => {
 
 Experience.propTypes = {
   experience: PropTypes.array.isRequired,
-}
+  deleteExperience: PropTypes.func.isRequired,
+};
 
-export default Experience
+export default connect(null, { deleteExperience })(Experience);
